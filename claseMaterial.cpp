fa5af98ef1c363a392206ddd26f7d6105d8b880f
Guts <aamamanica@unjbg.edu.pe>
2024-11-28 18:00:28 -0500

    Update claseMaterial.cpp

--- claseMaterial.cpp
+++ claseMaterial.cpp
@@ -1,9 +1,7 @@
 #include "claseMaterial.h"
-#include <iostream>
 using namespace std;
 
-
-Material::Material(string nombre, float kg) : nombre(nombre), kg(kg) {}
+Material::Material(std::string nombre, float kg) : nombre(nombre), kg(kg) {}
 
 float Material::getKg() { return kg; }
 string Material::getNombre() { return nombre; }
@@ -14,26 +12,22 @@ void Papel::mostrarInfo() {
     cout << "Material: " << nombre << ", Peso: " << kg << " kg" << endl;
 }
 
-Vidrio::Vidrio(float kg) : Material("Vidrio", kg) {} 
-
-void Vidrio::mostrarInfo(){
-    cout<< "Material: " << nombre << ", Peso: " << kg << "kg" <<endl; 
+Carton::Carton(float kg) : Material("Carton", kg) {}
+void Carton::mostrarInfo() {
+    cout << "Material: " << nombre << ", Peso: " << kg << " kg" << endl;
 }
 
-Carton::Carton(float kg) : Material("Carton", kg) {} 
-
-void Carton::mostrarInfo(){
-    cout<< "Material: " << nombre << ", Peso: " << kg << "kg" <<endl; 
+Vidrio::Vidrio(float kg) : Material("Vidrio", kg) {}
+void Vidrio::mostrarInfo() {
+    cout << "Material: " << nombre << ", Peso: " << kg << " kg" << endl;
 }
 
-Plastico::Plastico(float kg) : Material("Plastico", kg) {} 
-
-void Plastico::mostrarInfo(){
-    cout<< "Material: " << nombre << ", Peso: " << kg << "kg" <<endl; 
+Plastico::Plastico(float kg) : Material("Plastico", kg) {}
+void Plastico::mostrarInfo() {
+    cout << "Material: " << nombre << ", Peso: " << kg << " kg" << endl;
 }
 
-Metal::Metal(float kg) : Material("Metal", kg) {} 
-
-void Metal::mostrarInfo(){
-    cout<< "Material: " << nombre << ", Peso: " << kg << "kg" <<endl; 
+Metal::Metal(float kg) : Material("Metal", kg) {}
+void Metal::mostrarInfo() {
+    cout << "Material: " << nombre << ", Peso: " << kg << " kg" << endl;
 }
