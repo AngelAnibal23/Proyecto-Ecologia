5d4c7d27b680a1858fd6132413a0db68594d22d9
Guts <aamamanica@unjbg.edu.pe>
2024-11-28 18:02:00 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,7 @@
 #include "gestorReciclaje.h"
+#include "claseMaterial.h"
 #include <iostream>
-#include "funciones.cpp"
-#include "funciones.h"
-#include <vector>
-#include <string>
-
-using namespace std;
+using namespace std; 
 
 void mostrarMenu() {
     cout << "Gestor de Reciclaje\n";
@@ -14,6 +10,51 @@ void mostrarMenu() {
     cout << "3. Ordenar materiales por peso\n";
     cout << "4. Salir\n";
     cout << "Seleccione una opcion: ";
+}
+
+void menuMateriales(GestorReciclaje& gestor) {
+    int opc;
+    float kg;
+
+    cout << "\n_________________MENU MATERIALES_________________" << endl;
+    cout << "1.- Papel.\n";
+    cout << "2.- Carton.\n";
+    cout << "3.- Vidrio.\n";
+    cout << "4.- Plastico.\n";
+    cout << "5.- Metales.\n";
+    cout << "Ingrese una opcion: ";
+    cin >> opc;
+
+    switch (opc) {
+        case 1:
+            cout << "Ingrese el peso del papel: ";
+            cin >> kg;
+            gestor.agregarMaterial(new Papel(kg));
+            break;
+        case 2:
+            cout << "Ingrese el peso del carton: ";
+            cin >> kg;
+            gestor.agregarMaterial(new Carton(kg));
+            break;
+        case 3:
+            cout << "Ingrese el peso del vidrio: ";
+            cin >> kg;
+            gestor.agregarMaterial(new Vidrio(kg));
+            break;
+        case 4:
+            cout << "Ingrese el peso del plastico: ";
+            cin >> kg;
+            gestor.agregarMaterial(new Plastico(kg));
+            break;
+        case 5:
+            cout << "Ingrese el peso del metal: ";
+            cin >> kg;
+            gestor.agregarMaterial(new Metal(kg));
+            break;
+        default:
+            cout << "Opcion no valida.\n";
+            break;
+    }
 }
 
 int main() {
@@ -26,9 +67,8 @@ int main() {
 
         switch (opcion) {
             case 1: {
-                
-                menuMateriales(); 
-                
+                menuMateriales(gestor);
+                break;
             }
             case 2:
                 gestor.mostrarMateriales();
@@ -36,12 +76,12 @@ int main() {
             case 3:
                 gestor.ordenarPorKg();
                 cout << "Materiales ordenados por peso.\n";
-                break;
+                break; 
             case 4:
                 cout << "Saliendo del programa...\n";
                 break;
             default:
-                cout << "Opcion no valida.\n";
+                cout << "Opción no válida.\n";
                 break;
         }
     } while (opcion != 4);
